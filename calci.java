8433996c82921c522ec5dd8f8959317121b33b96
PawanKalyanHasthavaram <151132102+PawanKalyanHasthavaram@users.noreply.github.com>
2024-02-29 12:21:19 +0530

    calci.java

--- calci.java
+++ calci.java
@@ -2,7 +2,7 @@ interface Calci
 {
    public int add(int n1,int n2);
 }
-class calciImp implements calsi{
+class calciImp implements calci{
    public int add(int n1,int n2){
       return n1+n2;
    }
