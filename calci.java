d6e49e8bb4a29d0a86d1685f5f6a52942d9e2249
pawan kalyan <h.pawan123456@gmail.com>
2024-02-29 12:14:56 +0530

    modified by pawan

--- calci.java
+++ calci.java
@@ -1,4 +1,10 @@
 interface Calci
 {
-   public static int add(int n1,int n2);
+   public int add(int n1,int n2);
+}
+class calciImp implements calsi{
+   public int add(int n1,int n2){
+      return n1+n2;
+   }
+
 }
